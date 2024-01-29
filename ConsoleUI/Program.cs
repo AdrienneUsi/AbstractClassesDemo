e2a33b3b6f55a58c88c5e1fadac060e8d82b9d42
AdrienneUsi <usiadrienne35@gmail.com>
2024-01-29 12:17:28 -0800

    finish exercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +12,22 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             /*
-             * Todo: Follow all comments!! 
-             * Double click on the region (gray box) to view all comments
+             * Todo follow all comments!! 
              */
 
             #region Vehicles
 
             /*
              * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties: Year, Make, and Model
-             * Set the defaults of the properties to something generic in the Vehicle class
-             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
-             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
+             * The vehicle class shall have three string properties Year, Make, and Model
+             * Set the defaults to something generic in the Vehicle class
+             * Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
              * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
              * Provide the implementations for the abstract methods
              * Only in the Motorcycle class will you override the virtual drive method
             */
@@ -37,15 +37,48 @@ namespace ConsoleUI
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * 
-             * Set the properties values with object initializer syntax
+             * Set the properties with object initializer syntax
              */
+
+            var sedan = new Car();
+            sedan.Make = "Ford";
+            sedan.Model = "Fusion";
+            sedan.Year = "2021";
+            sedan.HasTrunk = true;
+
+            var harley = new Motorcycle();
+            harley.Make = "Harley";
+            harley.Model = "Big";
+            harley.Year = "2019";
+            harley.HasSideCart = false;
+
+            Vehicle toyota = new Car();
+            toyota.Make = "Toyota";
+            toyota.Model = "Camry";
+            toyota.Year = "2011";
+
+            Vehicle porshe = new Car();
+            porshe.Make = "Porshe";
+            porshe.Model = "911";
+            porshe.Year = "2015";
+
+            var vehicles = new List<Vehicle>() { porshe, toyota, harley, sedan };
 
             /*
              * Add the 4 vehicles to the list
-             * Using a foreach loop iterate through the list and display each of the properties
+             * Using a foreach loop iterate over each of the properties
              */
+            foreach (var ride in vehicles)
+            {
+                Console.WriteLine($"Make: {ride.Make}\nModel: {ride.Model}\nYear: {ride.Year}\n");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
+            sedan.DriveVirtual();
+            sedan.DriveAbstract();
+
+            harley.DriveAbstract();
+            harley.DriveVirtual();
 
             #endregion            
             Console.ReadLine();
